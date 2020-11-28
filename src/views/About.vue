b4b6518b6e0117ae38f9d1820d2a7f9a43353177
yczkhx <49805134+yczkhx@users.noreply.github.com>
2020-11-28 20:33:49 +0800

    日历

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,265 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <el-container>
+      <el-header>
+        <el-menu
+          :default-active="activeIndex2"
+          class="el-menu-demo"
+          mode="horizontal"
+          @select="handleSelect"
+        >
+          <div
+            class="el-icon-office-building"
+            style="
+              float: left;
+              margin-right: 20px;
+              font-size: 35px;
+              margin-top: 12px;
+            "
+          ></div>
+          <div
+            style="
+              float: left;
+              margin-right: 20px;
+              font-size: 28px;
+              font-family: STCaiyun;
+            "
+          >
+            马文博医院
+          </div>
+          <!-- <el-menu-item index="./about" style="float: left;margin: auto;"
+            ><div
+              class="el-icon-s-home"
+              style="
+                float: left;
+                margin-right: 8px;
+                font-size: 25px;
+                margin-top: 15px;
+              "
+            ></div>
+            首页</el-menu-item
+          > -->
+          <div
+            style="
+              float: right;
+              margin-right: 20px;
+              font-size: 20px;
+              font-family: FZShuTi;
+            "
+          >
+            马文博医生，您好
+          </div>
+        </el-menu>
+      </el-header>
+      <el-main>
+        <div class="demo-app">
+          <el-card class="box-card">
+            <div class="demo-app-sidebar">
+              <div class="demo-app-sidebar-section">
+                <h2>Instructions</h2>
+                <ul>
+                  <li>
+                    Select dates and you will be prompted to create a new event
+                  </li>
+                  <li>Drag, drop, and resize events</li>
+                  <li>Click an event to delete it</li>
+                </ul>
+              </div>
+              <div class="demo-app-sidebar-section">
+                <h2>All Events ({{ currentEvents.length }})</h2>
+                <ul>
+                  <li v-for="event in currentEvents" :key="event.id">
+                    <b>{{ event.startStr }}</b>
+                    <i>{{ event.title }}</i>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </el-card>
+          <div class="demo-app-main">
+            <FullCalendar class="demo-app-calendar" :options="calendarOptions">
+              <template v-slot:eventContent="arg">
+                <b>{{ arg.timeText }}</b>
+                <i>{{ arg.event.title }}</i>
+              </template>
+            </FullCalendar>
+          </div>
+        </div>
+      </el-main>
+    </el-container>
   </div>
 </template>
+
+
+<script>
+import FullCalendar from "@fullcalendar/vue";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+//import { INITIAL_EVENTS, createEventId } from './event-utils'
+import Element from "element-ui";
+
+let eventGuid = 0;
+let todayStr = new Date().toISOString().replace(/T.*$/, ""); // YYYY-MM-DD of today
+
+export const INITIAL_EVENTS = [
+  {
+    id: createEventId(),
+    title: "All-day event",
+    start: todayStr,
+  },
+  {
+    id: createEventId(),
+    title: "Timed event",
+    start: todayStr + "T12:00:00",
+  },
+];
+
+export function createEventId() {
+  return String(eventGuid++);
+}
+
+export default {
+  components: {
+    FullCalendar, // make the <FullCalendar> tag available
+  },
+
+  data: function () {
+    return {
+      calendarOptions: {
+        plugins: [
+          dayGridPlugin,
+          timeGridPlugin,
+          interactionPlugin, // needed for dateClick
+        ],
+        headerToolbar: {
+          left: "prev,next today",
+          center: "title",
+          right: "dayGridMonth,timeGridWeek,timeGridDay",
+        },
+        initialView: "dayGridMonth",
+        initialEvents: INITIAL_EVENTS, // alternatively, use the `events` setting to fetch from a feed
+        editable: true,
+        selectable: true,
+        selectMirror: true,
+        dayMaxEvents: true,
+        weekends: true,
+        select: this.handleDateSelect,
+        eventClick: this.handleEventClick,
+        eventsSet: this.handleEvents,
+        /* you can update a remote database when these fire:
+        eventAdd:
+        eventChange:
+        eventRemove:
+        */
+      },
+      currentEvents: [],
+    };
+  },
+
+  methods: {
+    handleWeekendsToggle() {
+      this.calendarOptions.weekends = !this.calendarOptions.weekends; // update a property
+    },
+
+    handleDateSelect(selectInfo) {
+      let title = prompt("Please enter a new title for your event");
+      let calendarApi = selectInfo.view.calendar;
+
+      calendarApi.unselect(); // clear date selection
+
+      if (title) {
+        calendarApi.addEvent({
+          id: createEventId(),
+          title,
+          start: selectInfo.startStr,
+          end: selectInfo.endStr,
+          allDay: selectInfo.allDay,
+        });
+      }
+    },
+
+    handleEventClick(clickInfo) {
+      if (
+        confirm(
+          `Are you sure you want to delete the event '${clickInfo.event.title}'`
+        )
+      ) {
+        clickInfo.event.remove();
+      }
+    },
+
+    handleEvents(events) {
+      this.currentEvents = events;
+    },
+  },
+};
+</script>
+
+
+<style scoped>
+.el-header {
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-main {
+  background-color: rgb(255, 255, 255);
+  color: #333;
+  text-align: center;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+
+h2 {
+  margin: 0;
+  font-size: 16px;
+}
+
+ul {
+  margin: 0;
+  padding: 0 0 0 1.5em;
+}
+
+li {
+  margin: 1.5em 0;
+  padding: 0;
+}
+
+b {
+  /* used for event dates/times */
+  margin-right: 3px;
+}
+
+.demo-app {
+  display: flex;
+  min-height: 100%;
+  font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
+  font-size: 14px;
+}
+
+.demo-app-sidebar {
+  width: 300px;
+  line-height: 1.5;
+  background: #eaf9ff;
+  border-right: 1px solid #d3e2e8;
+}
+
+.box-card {
+  background: #eaf9ff;
+}
+
+.demo-app-main {
+  flex-grow: 1;
+  padding: 3em;
+}
+
+.fc {
+  /* the calendar root */
+  max-width: 1100px;
+  margin: 0 auto;
+}
+</style>
