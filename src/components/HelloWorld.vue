0d8a0cc1a0bfe320cac298905fcafae560300c49
yczkhx <49805134+yczkhx@users.noreply.github.com>
2020-12-22 23:35:24 +0800

    新功能完成一半

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,7 +3,8 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>|
-      <router-link to="/opera">DoctorOpera</router-link>
+      <router-link to="/opera">DoctorOpera</router-link>|
+      <router-link to="/admin">Admin</router-link>
     </div>
   </div>
 </template>
---
+++ src/views/Admin.vue
@@ -0,0 +1,388 @@
+<template>
+  <div>
+    <el-container>
+      <el-header>
+        <el-menu class="el-menu-demo" mode="horizontal">
+          <div
+            class="el-icon-office-building"
+            style="
+              float: left;
+              margin-right: 20px;
+              font-size: 35px;
+              margin-top: 12px;
+            "
+          ></div>
+          <div
+            style="
+              float: left;
+              margin-right: 20px;
+              font-size: 28px;
+              font-family: STCaiyun;
+            "
+          >
+            马文博医院
+          </div>
+          <div
+            style="
+              float: right;
+              margin-right: 20px;
+              font-size: 20px;
+              font-family: FZShuTi;
+            "
+          >
+            马文博医生，您好
+          </div>
+        </el-menu>
+      </el-header>
+      <el-main>
+        <div class="demo-app">
+          <el-card class="box-card">
+            <div class="demo-app-sidebar">
+              <div class="demo-app-sidebar-section">
+                <h2>Set Timetable</h2>
+                <ul>
+                  <li>Here are the events you need to select</li>
+                  <li>Click it to set</li>
+                  <li>The follows are memebers</li>
+                </ul>
+              </div>
+              <div class="demo-app-sidebar-section" style="margin-top: 80px">
+                <h2>Choose your doctor</h2>
+                <el-select
+                  v-model="value"
+                  placeholder="请选择"
+                  style="margin-top: 80px"
+                  clearable
+                >
+                  <el-option
+                    v-for="item in options"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value"
+                  >
+                  </el-option>
+                </el-select>
+              </div>
+            </div>
+          </el-card>
+          <div class="demo-app-main">
+            <FullCalendar class="demo-app-calendar" :options="calendarOptions">
+              <template v-slot:eventContent="arg">
+                <b>{{ arg.timeText }}</b>
+                <i>{{ arg.event.title }}</i>
+              </template>
+            </FullCalendar>
+          </div>
+        </div>
+        <el-dialog title="提示" :visible.sync="visible" width="20%">
+          <span>确定选取这个时间段吗？</span>
+          <span slot="footer" class="dialog-footer">
+            <el-button @click="visible = false">取 消</el-button>
+            <el-button type="primary" @click="settheTime()">确 定</el-button>
+          </span>
+        </el-dialog>
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
+
+<script>
+import FullCalendar from "@fullcalendar/vue";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import allLocales from "@fullcalendar/core/locales-all";
+import listPlugin from "@fullcalendar/list";
+//import { INITIAL_EVENTS, createEventId } from './event-utils'
+import Element from "element-ui";
+import axios from "axios";
+
+let eventGuid = 0;
+let todayStr = new Date().toISOString().replace(/T.*$/, ""); // YYYY-MM-DD of today
+
+//console.log(todayStr + "T12:00:00")
+
+export default {
+  components: {
+    FullCalendar, // make the <FullCalendar> tag available
+  },
+
+  data: function () {
+    return {
+      calendarOptions: {
+        plugins: [
+          timeGridPlugin,
+          // needed for dateClick
+        ],
+        headerToolbar: {
+          left: "",
+          center: "title",
+          right: "timeGridWeek",
+        },
+        locale: "zh",
+        initialView: "timeGridWeek",
+        slotMinTime: "07:00:00",
+        slotMaxTime: "19:00:00",
+        //initialEvents: this.forINITIAL_EVENTS(), // alternatively, use the `events` setting to fetch from a feed
+        editable: true,
+        //selectable: true,
+        selectMirror: true,
+        dayMaxEvents: true,
+        height: "650px",
+        //weekends: true,
+        //navLinks: true,
+        //navLinkDayClick: "timeGridDay",
+        //nowIndicator: true,
+        // businessHours: [
+        //   // specify an array instead
+        //   {
+        //     daysOfWeek: [1, 2, 3, 4, 5], // Monday, Tuesday, Wednesday
+        //     startTime: "08:00", // 8am
+        //     endTime: "17:00", // 6pm
+        //   },
+        //   {
+        //     daysOfWeek: [0, 6], // Thursday, Friday
+        //     startTime: "09:00", // 10am
+        //     endTime: "16:00", // 4pm
+        //   },
+        // ],
+        weekends: true,
+        firstDay: 6,
+        //snapDuration: "00:10",
+        //select: this.handleDateSelect,
+        eventClick: this.handleEventClick,
+        //eventsSet: this.handleEvents,
+        //eventChange: this.handleEventChange,
+        //eventDataTransform: this.tranform,
+        events: this.forINITIAL_EVENTS(),
+        /* you can update a remote database when these fire:
+        eventAdd:
+        eventChange:
+        eventRemove:
+        */
+      },
+      options: this.allmembers(),
+      value: "",
+      visible: false,
+      currentEvents: [],
+      nowClickInfo: [],
+      newEventSelectInfo: [],
+      newCalendar: "",
+      nowCalendar: "",
+    };
+  },
+  mounted() {
+    //this.initialallView();
+    //this.forINITIAL_EVENTS();
+  },
+  methods: {
+    forINITIAL_EVENTS() {
+      var Events = [
+        {
+          id: "0",
+          title: "门诊",
+          start: "2020-12-21T08:00:00",
+          end: "2020-12-21T11:30:00",
+          color: "#ff9569",
+        },
+        {
+          id: "1",
+          title: "门诊",
+          start: "2020-12-21T14:00:00",
+          end: "2020-12-21T17:30:00",
+          color: "#409EFF",
+        },
+        {
+          id: "2",
+          title: "门诊",
+          start: "2020-12-22T08:00:00",
+          end: "2020-12-22T11:30:00",
+          color: "#409EFF",
+        },
+        {
+          id: "3",
+          title: "门诊",
+          start: "2020-12-22T14:00:00",
+          end: "2020-12-22T17:30:00",
+          color: "#409EFF",
+        },
+        {
+          id: "4",
+          title: "门诊",
+          start: "2020-12-23T08:00:00",
+          end: "2020-12-23T11:30:00",
+          color: "#409EFF",
+        },
+        {
+          id: "5",
+          title: "门诊",
+          start: "2020-12-23T14:00:00",
+          end: "2020-12-23T17:30:00",
+          color: "#409EFF",
+        },
+        {
+          id: "6",
+          title: "门诊",
+          start: "2020-12-24T08:00:00",
+          end: "2020-12-24T11:30:00",
+          color: "#409EFF",
+        },
+        {
+          id: "7",
+          title: "门诊",
+          start: "2020-12-24T14:00:00",
+          end: "2020-12-24T17:30:00",
+          color: "#409EFF",
+        },
+        {
+          id: "8",
+          title: "门诊",
+          start: "2020-12-25T08:00:00",
+          end: "2020-12-25T11:30:00",
+          color: "#409EFF",
+        },
+        {
+          id: "9",
+          title: "门诊",
+          start: "2020-12-25T14:00:00",
+          end: "2020-12-25T17:30:00",
+          color: "#409EFF",
+        },
+      ];
+      return Events;
+    },
+    handleEventClick(clickInfo) {
+      console.log(clickInfo);
+      this.visible = true;
+      this.nowClickInfo = clickInfo.event;
+      this.nowCalendar = clickInfo.view.calendar;
+    },
+    settheTime() {
+      if (this.value == "") {
+        this.$message.error("请选择你要添加的人员");
+        this.visible = false;
+        return;
+      }
+      this.nowCalendar
+        .getEventById(this.nowClickInfo.id)
+        .setProp("title", '已分配'); //修改标题
+        this.nowCalendar
+        .getEventById(this.nowClickInfo.id)
+        .setProp("color", '#ff9569'); //修改标题
+      var num = "";
+      if (this.nowClickInfo.startStr.slice(11, 19) == "08:00:00") {
+        num = 1;
+      } else if (this.nowClickInfo.startStr.slice(11, 19) == "14:00:00") {
+        num = 2;
+      }
+      axios
+        .get("http://localhost:8082/doctor/arr", {
+          params: {
+            doctor_id: this.value,
+            number: num,
+            date1: this.nowClickInfo.startStr.slice(0, 10),
+          },
+        })
+        .then((res) => {
+          console.log(res);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+      this.visible = false;
+    },
+
+    allmembers() {
+      var Members = [];
+      axios
+        .get("http://localhost:8082/doctor/get_member", {
+          params: {
+            doctor_id: "2020001",
+          },
+        })
+        .then((res) => {
+          //console.log(res);
+          for (var i in res.data) {
+            Members.push({
+              value: res.data[i].id,
+              label: res.data[i].name,
+            });
+          }
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+      return Members;
+    },
+  },
+};
+</script>
+
+
+<style scoped>
+.el-header {
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-main {
+  background-color: rgb(248, 248, 248);
+  color: #333;
+  text-align: center;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+
+h2 {
+  margin: 0;
+  font-size: 16px;
+}
+
+ul {
+  margin: 0;
+  padding: 0 0 0 1.5em;
+}
+
+li {
+  margin: 1.5em 0;
+  padding: 0;
+}
+
+b {
+  /* used for event dates/times */
+  margin-right: 3px;
+}
+
+.demo-app {
+  display: flex;
+  min-height: 100%;
+  font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
+  font-size: 14px;
+}
+
+.demo-app-sidebar {
+  width: 300px;
+
+  background: #eaf9ff;
+  border-right: 1px solid #d3e2e8;
+}
+
+.box-card {
+  background: #eaf9ff;
+}
+
+.demo-app-main {
+  flex-grow: 1;
+  padding: 3em;
+}
+
+.fc {
+  /* the calendar root */
+  max-width: 1100px;
+  margin: 0 auto;
+}
+</style>
